e0f6b3cd46c8701ffea0abd9fd7e8f78b8042c06
chuckloveall <chuckloveall@gmail.com>
2021-06-02 19:48:13 -0700

    fixed dice issue

--- static/js/static.js
+++ static/js/static.js
@@ -4,18 +4,24 @@ $('#roll').click(() => {
   let num = Math.floor(Math.random() * 6) + 1;
   let num2= Math.floor(Math.random() * 6) + 1;
   let cls = 'odd-'
+  let cls1= 'odd-'
   if (num % 2 === 0) {
-    cls = 'even-'
+    cls= 'even-'
   }
   if (num2 % 2 === 0) {
-      cls= 'even-'
+      cls1= 'even-'
   }
+  // console.log(num);
+  // console.log(num2);
+  // console.log(cls);
+  // console.log(cls1);
+
   $('#die').empty();
+  $('#die2').empty();
   for (let i = 1; i <= num; i++) {
     $('#die').append('<div class="dot ' + cls + i + '"></div>');
   }
-  $('#die2').empty();
   for (let i = 1; i <= num2; i++) {
-    $('#die2').append('<div class="dot ' + cls + i + '"></div>');
+    $('#die2').append('<div class="dot ' + cls1 + i + '"></div>');
   }
 });
