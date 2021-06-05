50e01961bfd81529830462cfb2d95f93a298f166
chuckloveall <chuckloveall@gmail.com>
2021-06-05 13:26:36 -0700

    added mozart midi files and switch case

--- static/js/static.js
+++ static/js/static.js
@@ -3,8 +3,10 @@
 $('#roll').click(() => {
   let num = Math.floor(Math.random() * 6) + 1;
   let num2= Math.floor(Math.random() * 6) + 1;
-  let cls = 'odd-'
-  let cls1= 'odd-'
+  let cls = 'odd-';
+  let cls1= 'odd-';
+  let sum1= num + num2;
+  let m1= 96;
   if (num % 2 === 0) {
     cls= 'even-'
   }
@@ -24,4 +26,58 @@ $('#roll').click(() => {
   for (let i = 1; i <= num2; i++) {
     $('#die2').append('<div class="dot ' + cls1 + i + '"></div>');
   }
+  switch(sum1) {
+      case 2:
+      m1= 96;
+      console.log(m1);
+      break
+      case 3:
+      m1= 32;
+      console.log(m1);
+      break
+      case 4:
+      m1= 69;
+      console.log(m1);
+      break
+      case 5:
+      m1= 40;
+      console.log(m1);
+      break
+      case 6:
+      m1= 148;
+      console.log(m1);
+      break
+      case 7:
+      m1= 104;
+      console.log(m1);
+      break
+      case 8:
+      m1= 152;
+      console.log(m1);
+      break
+      case 9:
+      m1= 119;
+      console.log(m1);
+      break
+      case 10:
+      m1= 98;
+      console.log(m1);
+      break
+      case 11:
+      m1= 3;
+      console.log(m1);
+      break
+      case 12:
+      m1= 54;
+      console.log(m1);
+      break
+  }
+  // player.("M${m1}.MID")
 });
+
+// save roll in variable
+// connect variable to midi File
+// pull midi file from library of files (measure from mozart)
+// midi file will be passed into model
+// model will generate new sequence based on primer
+//
